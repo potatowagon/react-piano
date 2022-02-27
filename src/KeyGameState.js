2ad8010fd2963c9f9f3acbde3c90d0d3ee443a6c
Sherry Wong <potatowagon@fb.com>
2022-02-27 17:23:50 +0800

    add keygamestate enum

---
+++ src/KeyGameState.js
@@ -0,0 +1,5 @@
+// TODO: change this to enum when typescript enabled
+export const UNSELECTED = Symbol("unselected")
+export const CORRECT = Symbol("correct")
+export const ABSENT = Symbol("absent")
+export const PRESENT = Symbol("present")
